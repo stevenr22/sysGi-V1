d96c9da9f8920c66be30af7cf07b4727b4594efa
stevenr22 <rojasguerreros185@gmail.com>
2025-06-17 14:56:26 -0500

    listo partes

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Header } from "../../components/partsDashboard/header/header";
+import { Navbar } from "../../components/partsDashboard/navbar/navbar";
+import { Main } from "../../components/partsDashboard/main/main";
+import { Footer } from "../../components/partsDashboard/footer/footer";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [Header, Navbar, Main, Footer],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
