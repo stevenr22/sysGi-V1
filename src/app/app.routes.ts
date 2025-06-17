bbf414e700a6e0bfbe15456f5cfbdf24eef3970c
stevenr22 <rojasguerreros185@gmail.com>
2025-06-17 16:52:29 -0500

    list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
 import { Register } from './auth/register/register';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { NotFound } from './components/not-found/not-found';
 
 export const routes: Routes = [
     {
         path: '',
         redirectTo: 'login',
-        pathMatch: 'full' //sirve para entrar a la raiz que esta redirigiendo el login.
+        pathMatch: 'full'
     },
     {
         path: 'login',
@@ -17,9 +18,12 @@ export const routes: Routes = [
         path: 'register',
         component: Register
     },
-    
     {
         path: 'dashboard',
         component: Dashboard
+    },
+    {
+        path: '**',
+        component: NotFound
     }
 ];
