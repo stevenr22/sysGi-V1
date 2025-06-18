31369dc6364f6a28e76c9de916a2becb0ed7cfd9
stevenr22 <rojasguerreros185@gmail.com>
2025-06-18 16:15:58 -0500

    listo

--- src/app/shared/components/botonG/boton-g.ts
+++ src/app/shared/components/botonG/boton-g.ts
@@ -10,14 +10,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class BotonG {
   @Input() type: 'button' | 'submit' | 'reset' = 'button';
   @Input() color: string = 'primary'; // valor por defecto
-  @Input() size: string = 'lg';       // valor por defecto
   @Input() btnClass: string = '';
   @Input() disabled: boolean = false;
   @Output() onClick = new EventEmitter<Event>();
 
   get classes(): string {
     // Construye las clases de Bootstrap dinámicamente
-    return `btn btn-${this.color} btn-${this.size} ${this.btnClass}`;
+    return `btn btn-${this.color} ${this.btnClass}`;
   }
 
 }
